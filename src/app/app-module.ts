fb05c54773ec90b55747ed8b551977ef7a8cacf2
Nino <fazernino@gmail.com>
2025-09-11 15:14:54 +0200

    🔧 init config

---
+++ src/app/app-module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './shared/material/material-module';
+
+@NgModule({
+  declarations: [],
+  imports: [MaterialModule],
+})
+export class AppModule {}
