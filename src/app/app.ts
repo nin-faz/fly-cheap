4bd60ef94474063cb084036be6ef20c214bb22a3
Nino <fazernino@gmail.com>
2025-09-12 12:02:10 +0200

    ✨ added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header';
-
+import { FooterComponent } from './shared/components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
+    <div class="h-[30.5em] bg-red-500"></div>
+    <app-footer></app-footer>
   `,
   styleUrls: [],
 })
