904b96e1060f5135b00960447878947e38cafde6
Nino <fazernino@gmail.com>
2025-09-12 11:40:42 +0200

    ✨ added header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: [],
 })
 export class App {
   protected readonly title = signal('fly-cheap');
